5a72d02c08412ba11d514d2c56b5a2827d93d00c
Joe Rouleau <jrouleau@avfx.com>
2019-05-09 10:57:38 -0400

    added property to User class for tracking access across system

--- app/model/user.php
+++ app/model/user.php
@@ -6,9 +6,16 @@
 //  Created by Joe Rouleau on 4/29/19.
 //
 
+abstract class UserType
+{
+    const Admin = 0;
+    const Basic = 1;
+}
+
 class User {
     public $emailAddress;
     public $password;
+    public $userType;   //  UserType
 
     function __construct($emailAddress, $password) {
         $this->emailAddress = $emailAddress;
